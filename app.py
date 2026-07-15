9546b8e876b8a1ed940d8c60ccb03ed72a02cdfe
varsha <varshabs_gy27@mysururoyal.org>
2026-07-15 15:19:38 +0530

    completed

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
+import mysql.connector
 from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+def get_products():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="29032005",
+        database="productdb"
+    )
+
+    cur = conn.cursor(dictionary=True)
+    cur.execute("SELECT id, name, price FROM products")
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return rows
 
 @app.route('/')
-
 def home():
-     return render_template('index.html')
+    return render_template('index.html', products=get_products())
 
-if __name__=='__main__':
-
-   app.run(debug=True,port=5001)
+if __name__ == '__main__':
+    app.run(debug=True)
